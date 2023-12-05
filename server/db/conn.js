26e3265b81813c0cf4f18a17de1807dd66b665d6
jwke21 <jakevm21@gmail.com>
2023-12-05 14:53:03 -0800

    updated server code

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,25 +1,29 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   },
- 
+});
+
+var _db;
+
+async function connectToServer(callback) {
+  // connect to the server
+  await client.connect(function (err, db) {
+    // Verify we got a good "db" object
+    if (db) {
+      // _db = db.db("employees");
+      console.log("Successfully connected to MongoDB.");
+    }
+    return callback(err);
+  });
+}
+
+module.exports = {
+  connectToServer,
   getDb: function () {
     return _db;
   },
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config({ path: "./config.env" });
 
 const port = process.env.PORT || 5000;
@@ -12,10 +13,10 @@ app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
 
-app.listen(port, () => {
+app.listen(port, async () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
+  await dbo.connectToServer(function (err) {
     if (err) console.error(err);
-   });
+  });
   console.log(`Server is running on port: ${port}`);
 });
